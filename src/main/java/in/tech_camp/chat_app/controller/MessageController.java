b4857e2330cdc0f0b264183a96de7c5b37416d2d
kotaro-sugiyama <kotaro.sugiyama@smart-group.co.jp>
2026-05-13 17:24:11 +0900

    メッセージの表示

--- src/main/java/in/tech_camp/chat_app/controller/MessageController.java
+++ src/main/java/in/tech_camp/chat_app/controller/MessageController.java
@@ -48,6 +48,8 @@ import lombok.AllArgsConstructor;
         model.addAttribute("messageForm", new MessageForm());
         model.addAttribute("roomId", roomId);
 
+        List<MessageEntity> messages = messageRepository.findByRoomId(roomId);
+        model.addAttribute("messages", messages);
         return "messages/index";
        }
 
