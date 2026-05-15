0f5c002e351a3f617e5a8963ad5a5600b3615883
kotaro-sugiyama <kotaro.sugiyama@smart-group.co.jp>
2026-05-15 10:31:14 +0900

    各フォームのテスト

--- src/main/java/in/tech_camp/chat_app/form/UserForm.java
+++ src/main/java/in/tech_camp/chat_app/form/UserForm.java
@@ -11,22 +11,22 @@ package in.tech_camp.chat_app.form;
 
   @Data
   public class UserForm {
-    @NotBlank(message = "Name can't be blank", groups = ValidationPriority1.class)
-    private String name;  
+  @NotBlank(message = "Name can't be blank", groups = ValidationPriority1.class)
+  private String name;
 
-    @NotBlank(message = "Email can't be blank", groups = ValidationPriority1.class)
-    @Email(message = "Email should be valid", groups = ValidationPriority2.class)
-    private String email;
+  @NotBlank(message = "Email can't be blank", groups = ValidationPriority1.class)
+  @Email(message = "Email should be valid", groups = ValidationPriority2.class)
+  private String email;
 
-    @NotBlank(message = "Password can't be blank", groups = ValidationPriority1.class)
-    @Length(min = 6, max = 128, message = "Password should be between 6 and 128 characters", groups = ValidationPriority2.class)
-    private String password;
+  @NotBlank(message = "Password can't be blank", groups = ValidationPriority1.class)
+  @Length(min = 6, max = 128, message = "Password should be between 6 and 128 characters", groups = ValidationPriority2.class)
+  private String password;
+  
+  private String passwordConfirmation;
 
-    private String passwordConfirmation;
-
-    public void validatePasswordConfirmation(BindingResult result) {
-      if (!password.equals(passwordConfirmation)) {
-        result.rejectValue("passwordConfirmation", "error.user", "Password .confirmation doesn't match Password");
-      }
+  public void validatePasswordConfirmation(BindingResult result) {
+    if (!password.equals(passwordConfirmation)) {
+      result.rejectValue("passwordConfirmation", "error.user", "Password confirmation doesn't match Password");
     }
+  }
 }
--- src/main/java/in/tech_camp/chat_app/validation/ValidationOrder.java
+++ src/main/java/in/tech_camp/chat_app/validation/ValidationOrder.java
@@ -3,6 +3,6 @@ package in.tech_camp.chat_app.validation;
   import jakarta.validation.GroupSequence;
 
   @GroupSequence({ ValidationPriority1.class, ValidationPriority2.class})
-  public class ValidationOrder {
+  public interface  ValidationOrder {
   
 }
